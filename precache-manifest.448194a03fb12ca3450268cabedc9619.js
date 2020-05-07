d07458f10541194d39b72171064f93a138c366c1
Connor Brookes <connor.brookes@student.manchester.ac.uk>
2020-05-07 10:35:23 +0100

    Updates

---
+++ precache-manifest.448194a03fb12ca3450268cabedc9619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a49d55cd63c225d0dcc11954fd8f189",
+    "url": "/self-portfolio/index.html"
+  },
+  {
+    "revision": "d822b2a2ec710e2d051b",
+    "url": "/self-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0a12901a4ee733209170",
+    "url": "/self-portfolio/static/js/2.c4714809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/self-portfolio/static/js/2.c4714809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d822b2a2ec710e2d051b",
+    "url": "/self-portfolio/static/js/main.68c2c161.chunk.js"
+  },
+  {
+    "revision": "def605c2f8897503df83",
+    "url": "/self-portfolio/static/js/runtime-main.a031a9d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/self-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
